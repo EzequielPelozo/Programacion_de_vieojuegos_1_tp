4d30f75738b4e1442ee0a664151e86b7fc428fbd
Fiorella Aquino <65473103+FiorellaAV@users.noreply.github.com>
2024-11-11 19:52:56 -0300

    se agrego spritesheet, borro archivos viejos y actualizar package

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -4,3 +4,14 @@ export function lerp(a, b, t) {
     
     return a + (b - a) * t;
 }
+
+export function calculateDistance(x1, y1, x2, y2) {
+    const dx = Math.abs(x2 - x1);
+    const dy = Math.abs(y2 - y1);
+
+    if (dx > dy) {
+        return dx + 0.4 * dy;
+    } else {
+        return dy + 0.4 * dx;
+    }
+}
